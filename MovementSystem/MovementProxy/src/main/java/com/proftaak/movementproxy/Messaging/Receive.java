4a9ff23c984372744449661dada33971e8a10826
Bram Teurlings <b.teurlings@student.fontys.nl>
2019-04-16 11:32:56 +0200

    -Added GSON parsing and logging.

--- MovementSystem/MovementProxy/src/main/java/com/proftaak/movementproxy/Messaging/Receive.java
+++ MovementSystem/MovementProxy/src/main/java/com/proftaak/movementproxy/Messaging/Receive.java
@@ -1,7 +1,10 @@
 package com.proftaak.movementproxy.Messaging;
 
+import com.google.gson.Gson;
+import com.proftaak.movementregistrationservice.shared.MovementMessage;
 import com.proftaak.rabbitmq.ConnectionFactory;
 import com.rabbitmq.client.*;
+import com.sun.media.jfxmedia.logging.Logger;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
@@ -14,15 +17,17 @@ import java.util.concurrent.TimeoutException;
 public class Receive {
     private final static String QUEUE_NAME = "MovementRegistration_To_MovementProxy";
     private final static String QUEUE_NAME2 = "Simulation_To_MovementProxy";
+    private Gson gson = new Gson();
+
 
     @PostConstruct
     public void main() {
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-        initQueue(QUEUE_NAME);
-        initQueue(QUEUE_NAME2);
+        initQueue(QUEUE_NAME, false);
+        initQueue(QUEUE_NAME2, true);
     }
 
-    private void initQueue(String queueName){
+    private void initQueue(String queueName, boolean simulation){
         ConnectionFactory factory = new ConnectionFactory();
         Connection connection = null;
         Channel channel = null;
@@ -32,7 +37,11 @@ public class Receive {
             channel = connection.createChannel();
             channel.queueDeclare(queueName, false, false, false, null);
             System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-            startReceiving(channel, queueName);
+            if(simulation){
+                startReceivingSimulation(channel, queueName);
+            }else{
+                startReceiving(channel, queueName);
+            }
         } catch (IOException | TimeoutException e) {
             e.printStackTrace();
         }
@@ -45,6 +54,23 @@ public class Receive {
                 throws IOException {
                 String message = new String(body, "UTF-8");
                 System.out.println(" [x] received '" + message + "'");
+            }
+        };
+        try {
+            channel.basicConsume(queueName, true, consumer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startReceivingSimulation(Channel channel, String queueName){
+        Consumer consumer = new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                    throws IOException {
+                String message = new String(body, "UTF-8");
+                MovementMessage movementMessage =  gson.fromJson(message, MovementMessage.class);
+                Logger.logMsg(1, movementMessage.toString());
             }
         };
         try {
