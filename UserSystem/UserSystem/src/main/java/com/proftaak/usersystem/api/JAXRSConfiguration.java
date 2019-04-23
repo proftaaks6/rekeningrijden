0ca829855837e899492266a303d809f842672515
Carlo Theunissen <carlotheunissen96@gmail.com>
2019-04-23 11:32:56 +0200

    fix pom and add api

---
+++ UserSystem/UserSystem/src/main/java/com/proftaak/usersystem/api/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.proftaak.usersystem.api;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures a JAX-RS endpoint. Delete this class, if you are not exposing
+ * JAX-RS resources in your application.
+ */
+@ApplicationPath(value = "/v1")
+class JAXRSConfiguration extends Application {
+
+}
