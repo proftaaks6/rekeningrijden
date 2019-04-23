3bd0cd27e25df8bf0b860126d1238649ccf0b5a3
Carlo Theunissen <carlotheunissen96@gmail.com>
2019-04-23 12:51:19 +0200

    resolve conflict in domain

--- _Tools/start.sh
+++ _Tools/start.sh
@@ -6,4 +6,6 @@ echo "##################"
 echo "#### Please note: Make sure you have recompiled the sources. This script only instructs payara to load new classes"
 echo "###################"
 echo
-docker-compose -f ../docker-compose.yml -f ../docker-compose.dev.yml up --build -d
+docker-compose -f ../docker-compose.yml -f ../docker-compose.dev.yml up -d db
+sleep 20
+docker-compose -f ../docker-compose.yml -f ../docker-compose.dev.yml up -d
