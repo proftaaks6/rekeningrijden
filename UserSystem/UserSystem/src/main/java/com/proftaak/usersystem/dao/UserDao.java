7c383f1aa26a3d49b2386a3e74bdc03ee8b1b017
rickjeurissen <computergek1996>
2019-04-23 10:50:26 +0200

    pre merge changes

--- UserSystem/UserSystem/src/main/java/com/proftaak/usersystem/dao/UserDao.java
+++ UserSystem/UserSystem/src/main/java/com/proftaak/usersystem/dao/UserDao.java
@@ -1,5 +1,5 @@
 package com.proftaak.usersystem.dao;
 
 public interface UserDao {
-
+    void saveUserInformation(String name, String email, String address, String residence);
 }
---
+++ UserSystem/UserSystem/src/main/java/com/proftaak/usersystem/dao/impl/UserDaoImpl.java
@@ -0,0 +1,10 @@
+package com.proftaak.usersystem.dao.impl;
+
+import com.proftaak.usersystem.dao.UserDao;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public void saveUserInformation(String name, String email, String address, String residence) {
+
+    }
+}
