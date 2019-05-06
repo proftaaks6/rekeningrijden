fccef6a415b3b5c5ab2313c75ac114a8c0618c3d
Wouter <woutervanacht@hotmail.com>
2019-05-06 19:38:15 +0200

    Adding of users through government system

--- GovernmentSystem/GovernmentAdmin/src/main/java/com/proftaak/governmentadmin/service/GovernmentAdminService.java
+++ GovernmentSystem/GovernmentAdmin/src/main/java/com/proftaak/governmentadmin/service/GovernmentAdminService.java
@@ -1,13 +1,51 @@
 package com.proftaak.governmentadmin.service;
 
 import com.proftaak.usersystem.shared.ClientUser;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.Stateless;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import sun.net.www.http.HttpClient;
 
 @Stateless
 public class GovernmentAdminService
 {
-    public ClientUser addUser(String name, String email) {
-        // TODO: Send user to UserSystem and persist
+    public ClientUser addUser(String name,
+                              String address,
+                              String residence,
+                              String email) throws IOException
+    {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpPost httppost = new HttpPost("http://localhost:8083/deploy/v1/usersystem/userInfo");
+
+        // Request parameters and other properties.
+        List<NameValuePair> params = new ArrayList<NameValuePair>(2);
+        params.add(new BasicNameValuePair("name", name));
+        params.add(new BasicNameValuePair("email", email));
+        params.add(new BasicNameValuePair("residence", residence));
+        params.add(new BasicNameValuePair("address", address));
+        httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+
+        //Execute and get the response.
+        CloseableHttpResponse response = httpclient.execute(httppost);
+        HttpEntity entity = response.getEntity();
+
+        if (entity != null) {
+            try (InputStream instream = entity.getContent()) {
+                // return entity
+            }
+        }
+
         return null;
     }
 }
