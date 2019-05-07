297df0d0a610f844b57ff4f406b4750589357ded
Bram Teurlings <b.teurlings@student.fontys.nl>
2019-05-07 14:49:16 +0200

    -Changed queue initializations

--- MovementSystem/MovementRegistrationService/src/main/java/com/proftaak/movementregistrationservice/Messaging/Send.java
+++ MovementSystem/MovementRegistrationService/src/main/java/com/proftaak/movementregistrationservice/Messaging/Send.java
@@ -1,11 +1,9 @@
 package com.proftaak.movementregistrationservice.Messaging;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.proftaak.movementregistrationservice.shared.Coordinate;
-import com.proftaak.movementregistrationservice.shared.MovementMessage;
 import com.proftaak.rabbitmq.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
+import com.sun.media.jfxmedia.logging.Logger;
 import org.apache.log4j.BasicConfigurator;
 
 import javax.annotation.PostConstruct;
@@ -13,31 +11,36 @@ import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
 
-@Singleton
-@Startup
 public class Send {
     private final static String QUEUE_NAME = "MovementRegistration_To_MovementProxy";
+    private Connection connection;
+    private Channel channel;
 
-    @PostConstruct
-    public void main() {
+    public Send() {
+        try {
+            BasicConfigurator.configure();
+            ConnectionFactory factory = new ConnectionFactory();
+            this.connection = factory.newConnection();
+            this.channel = connection.createChannel();
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Message queue initialized between MovementProxy and MovementRegistration.";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(message);
+        } catch (IOException | TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage(String message){
         BasicConfigurator.configure();
         ConnectionFactory factory = new ConnectionFactory();
-        try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-
-            //Todo: Test code, can be removed once validated
-            MovementMessage movementMessage = new MovementMessage(-1, new Coordinate(1, 2));
-            ObjectMapper mapper = new ObjectMapper();
-            String message = mapper.writeValueAsString(movementMessage);
-
-            //Todo: Remove
-            for(int i = 0; i < 1000; i++){
-                channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-            }
-
-            System.out.println(message);
-        } catch (TimeoutException | IOException e) {
+        try {
+            this.channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            this.channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println("Sent message: " + message);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
