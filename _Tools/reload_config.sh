29eccd5536a2eb536376fc83268f98a0a0fe0477
Carlo Theunissen <carlotheunissen96@gmail.com>
2019-05-14 12:17:03 +0200

    add driver frontend

--- _Tools/reload_config.sh
+++ _Tools/reload_config.sh
@@ -2,5 +2,5 @@ echo Stop script started!
 echo ""
 echo RUNNING IN DEVELOPMENT
 echo ""
-docker-compose -f "../docker-compose.yml" -f "../docker-compose.dev.yml" build $1
+docker-compose -f "../docker-compose.yml" -f "../docker-compose.dev.yml" "build" $1
 docker-compose -f "../docker-compose.yml" -f "../docker-compose.dev.yml" up --no-deps -d $1
