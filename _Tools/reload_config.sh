b4c5d1f1364f42e4282051df1b284e3c9d6c8ff6
Carlo Theunissen <carlotheunissen96@gmail.com>
2019-05-14 12:58:04 +0200

    invoice system in docker

--- _Tools/reload_config.sh
+++ _Tools/reload_config.sh
@@ -1,6 +1,3 @@
-echo Stop script started!
-echo ""
-echo RUNNING IN DEVELOPMENT
-echo ""
-docker-compose -f "../docker-compose.yml" -f "../docker-compose.dev.yml" "build" $1
-docker-compose -f "../docker-compose.yml" -f "../docker-compose.dev.yml" up --no-deps -d $1
+echo Reload Config script started!
+docker-compose -f ../docker-compose.yml -f ../docker-compose.dev.yml build $1
+docker-compose -f ../docker-compose.yml -f ../docker-compose.dev.yml up --no-deps -d $1
