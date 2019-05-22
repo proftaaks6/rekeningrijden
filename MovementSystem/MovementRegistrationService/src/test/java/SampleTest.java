742804da40dfa4b6a17f50e22cff391a49ecb419
Carlo Theunissen <carlotheunissen96@gmail.com>
2019-05-22 09:22:28 +0200

    add sample tests

---
+++ MovementSystem/MovementRegistrationService/src/test/java/SampleTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class SampleTest {
+
+    @Test
+    void sample(){
+        assertThat( "Movement Registration Service werkt!", true);
+    }
+}
---
+++ UserSystem/UserSystem/src/test/java/SampleTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class SampleTest {
+
+    @Test
+    void sample(){
+        assertThat( "User System werkt!", true);
+    }
+}
