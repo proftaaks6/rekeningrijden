657f6b6c26d5ff91e8a2dbe2952aa086e11508b8
Carlo Theunissen <carlotheunissen96@gmail.com>
2019-05-24 13:22:34 +0200

    auto generation of domain.xml for kubernetis

--- _Tools/copy_docker_files.sh
+++ _Tools/copy_docker_files.sh
@@ -8,5 +8,11 @@ cp -Rf ../docker_samples/. ../MovementSystem/MovementProxy/
 cp -Rf ../docker_samples/. ../MovementSystem/MovementRegistrationService/
 cp -Rf ../docker_samples/. ../UserSystem/UserSystem/
 cp -Rf ../docker_samples/. ../DriverSystem/DriverApplication/
-cp -Rf ../docker_samples/. ../InvoiceSystem/InvoiceSystem/
 
+cd ../DriverSystem/DriverApplication/ && sed -i '' 's/_DATABASE_/driverapplicationdatabase/g' domain.xml && cd ../../_Tools
+cd ../GovernmentSystem/GovernmentAdmin/ && sed -i '' 's/_DATABASE_/governmentdatabase/g' domain.xml && cd ../../_Tools
+cd ../InvoiceSystem/InvoiceSystem/ && sed -i '' 's/_DATABASE_/invoicedatabase/g' domain.xml && cd ../../_Tools
+cd ../MovementSystem/MovementProxy/ && sed -i '' 's/_DATABASE_/invaliddatadatabase/g' domain.xml && cd ../../_Tools
+cd ../MovementSystem/MovementRegistrationService/ && sed -i '' 's/_DATABASE_/movementdatabase/g' domain.xml && cd ../../_Tools
+cd ../UserSystem/UserSystem/ && sed -i '' 's/_DATABASE_/userdatabase/g' domain.xml && cd ../../_Tools
+cd ../DriverSystem/DriverApplication/ && sed -i '' 's/_DATABASE_/driverapplicationdatabase/g' domain.xml && cd ../../_Tools
