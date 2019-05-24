13b45190c77556b23a0c76a9e182559e678a8937
Carlo Theunissen <carlotheunissen96@gmail.com>
2019-05-24 13:37:06 +0200

    remove ''

--- _Tools/copy_docker_files.sh
+++ _Tools/copy_docker_files.sh
@@ -9,10 +9,10 @@ cp -Rf ../docker_samples/. ../MovementSystem/MovementRegistrationService/
 cp -Rf ../docker_samples/. ../UserSystem/UserSystem/
 cp -Rf ../docker_samples/. ../DriverSystem/DriverApplication/
 
-sed -i '' 's/_DATABASE_/driverapplicationdatabase/g' ../DriverSystem/DriverApplication/domain.xml
-sed -i '' 's/_DATABASE_/governmentdatabase/g' ../GovernmentSystem/GovernmentAdmin/domain.xml
-sed -i '' 's/_DATABASE_/invoicedatabase/g' ../InvoiceSystem/InvoiceSystem/domain.xml
-sed -i '' 's/_DATABASE_/invaliddatadatabase/g' ../MovementSystem/MovementProxy/domain.xml
-sed -i '' 's/_DATABASE_/movementdatabase/g' ../MovementSystem/MovementRegistrationService/domain.xml
-sed -i '' 's/_DATABASE_/userdatabase/g' ../UserSystem/UserSystem/domain.xml
-sed -i '' 's/_DATABASE_/driverapplicationdatabase/g' ../DriverSystem/DriverApplication/domain.xml
+sed -i 's/_DATABASE_/driverapplicationdatabase/g' ../DriverSystem/DriverApplication/domain.xml
+sed -i 's/_DATABASE_/governmentdatabase/g' ../GovernmentSystem/GovernmentAdmin/domain.xml
+sed -i 's/_DATABASE_/invoicedatabase/g' ../InvoiceSystem/InvoiceSystem/domain.xml
+sed -i 's/_DATABASE_/invaliddatadatabase/g' ../MovementSystem/MovementProxy/domain.xml
+sed -i 's/_DATABASE_/movementdatabase/g' ../MovementSystem/MovementRegistrationService/domain.xml
+sed -i 's/_DATABASE_/userdatabase/g' ../UserSystem/UserSystem/domain.xml
+sed -i 's/_DATABASE_/driverapplicationdatabase/g' ../DriverSystem/DriverApplication/domain.xml
